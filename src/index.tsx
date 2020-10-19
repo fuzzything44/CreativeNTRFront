41920b94915d9f0761e7a98f4b5a30391e4b1f57
fuzzything44 <slimesftw@gmail.com>
2020-10-18 20:50:21 -0600

    Fix formatting in index.html, index.tsx, add node processing (#3)

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import { HelloComponent } from './hello';
+import { setup } from './setup';
 
-    ReactDOM.render(
-        <HelloComponent/>,
-        document.getElementById('root')
-    );
+setup();
+ReactDOM.render(
+    <HelloComponent/>,
+    document.getElementById('root')
+);
+
